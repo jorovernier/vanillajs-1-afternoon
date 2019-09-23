0ca27f5a5ab3d051111a13c352b1d8c40f935863
Joely Vernier <55214971+jorovernier@users.noreply.github.com>
2019-09-23 15:20:18 -0700

    Finished steps, not bd

---
+++ Part2/index.js
@@ -0,0 +1,12 @@
+const idInput = document.getElementById('idInput');
+const colorInput = document.getElementById('colorInput');
+
+function setCard(){
+    const card = document.getElementById
+    (idInput.value);
+    card.style.color = colorInput.value;
+}
+
+// function resetCards(){
+//     replace(idInput.style.color);
+// }
