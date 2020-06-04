8a2edfe5480da655fd8c8b2a19d0c667c66a1d8d
jorovernier <55214971+jorovernier@users.noreply.github.com>
2020-06-04 13:08:26 -0700

    finished black diamonds

--- Part1/index.js
+++ Part1/index.js
@@ -2,16 +2,24 @@ const board = [];
 
 function play(clickedId){
     const playerSpan = document.getElementById("player");
-    const clickedElement = document.getElementById(clickedId)
+    const clickedElement = document.getElementById(clickedId);
 
         if (playerSpan.innerText === 'X'){
-            playerSpan.innerText = 'O';
-            clickedElement.innerText = 'X';
-            board[clickedId] = 'X';
+            if(clickedElement.innerText === '') {
+                playerSpan.innerText = 'O';
+                clickedElement.innerText = 'X';
+                board[clickedId] = 'X';
+            } else {
+                alert('That spot is already taken!');
+            }
         } else {
-            playerSpan.innerText = 'X';
-            clickedElement.innerText = 'O';
-            board[clickedId] = 'O';
+            if(clickedElement.innerText === '') {
+                playerSpan.innerText = 'X';
+                clickedElement.innerText = 'O';
+                board[clickedId] = 'O';
+            } else {
+                alert('That spot is already taken!');
+            }
         }
         console.log(board);
 
@@ -28,56 +36,78 @@ function play(clickedId){
         if (topLeft !== undefined && topLeft === 
         upperMiddle && topLeft === topRight) {
             alert(`${topLeft} is the winner!`);
-            return;
+            return reset();
         }
         if (midLeft !== undefined && midLeft ===
         trueMiddle && midLeft === midRight) {
             alert(`${midLeft} is the winner!`);
-            return;
+            return reset();
         }
         if (bottomLeft !== undefined && bottomLeft ===
         lowerMiddle && bottomLeft === bottomRight) {
             alert(`${bottomLeft} is the winner!`);
-            return;
+            return reset();
         }
         if (topLeft !== undefined && topLeft === 
         midLeft && topLeft === bottomLeft) {
             alert(`${topLeft} is the winner!`);
-            return;
+            return reset();
         }
         if (upperMiddle !== undefined && upperMiddle === 
             trueMiddle && upperMiddle === lowerMiddle) {
             alert(`${upperMiddle} is the winner!`);
-            return;
+            return reset();
         }
         if (topRight !== undefined && topRight === 
         midRight && topRight === bottomRight) {
             alert(`${topRight} is the winner!`);
-            return;
+            return reset();
         }
         if (topLeft !== undefined && topLeft === 
         trueMiddle && topLeft === bottomRight) {
             alert(`${topLeft} is the winner!`);
-            return;
+            return reset();
         }
         if (bottomLeft !== undefined && bottomLeft === 
         trueMiddle && bottomLeft === topRight) {
             alert(`${bottomLeft} is the winner!`);
-            return;
+            return reset();
         }
 
     let boardFull = true;
+    
         for (let i = 0; i <= 8; i++) {
-        if (board[i] === undefined) {
-        boardFull = false;
-        }
-
+            if (board[i] === undefined) {
+            boardFull = false;
+            }
         }
         if (boardFull === true) {
-        alert("It's a tie!");
+        alert("It's a tie!")
+        return playerSpan.innerText = '',
+            clickedElement.innerText = '',
+            board[clickedId] = '';
         }
 }
 
-// function reset(){
-//     delete clickedElement.innerText;
-// }
+function reset() {
+    const clickedElement0 = document.getElementById(0);
+    const clickedElement1 = document.getElementById(1);
+    const clickedElement2 = document.getElementById(2);
+    const clickedElement3 = document.getElementById(3);
+    const clickedElement4 = document.getElementById(4);
+    const clickedElement5 = document.getElementById(5);
+    const clickedElement6 = document.getElementById(6);
+    const clickedElement7 = document.getElementById(7);
+    const clickedElement8 = document.getElementById(8);
+    return clickedElement0.innerText = '',
+            clickedElement1.innerText = '',
+            clickedElement2.innerText = '',
+            clickedElement3.innerText = '',
+            clickedElement4.innerText = '',
+            clickedElement5.innerText = '',
+            clickedElement6.innerText = '',
+            clickedElement7.innerText = '',
+            clickedElement8.innerText = ''
+}
+
+    
--- Part2/index.js
+++ Part2/index.js
@@ -2,11 +2,17 @@ const idInput = document.getElementById('idInput');
 const colorInput = document.getElementById('colorInput');
 
 function setCard(){
-    const card = document.getElementById
-    (idInput.value);
+    const card = document.getElementById(idInput.value);
     card.style.color = colorInput.value;
 }
 
-// function resetCards(){
-//     replace(idInput.style.color);
-// }
+function resetCards(){
+    const diamonds = document.getElementById('diamonds');
+    const clubs = document.getElementById('clubs');
+    const hearts = document.getElementById('hearts');
+    const spades = document.getElementById('spades');
+    diamonds.style.color = '';
+    clubs.style.color = '';
+    hearts.style.color = '';
+    spades.style.color = '';
+}
